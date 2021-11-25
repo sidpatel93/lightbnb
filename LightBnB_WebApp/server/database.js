099729131030129afb1da8f369a2e9516028e14c
spatel93 <sid.auburn@gmail.com>
2021-11-25 16:14:29 +0000

    add logic to add new properties to the database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -144,11 +144,11 @@ const getAllProperties = function(options, limit = 10) {
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
-  console.log(options)
-  console.log(queryParams)
 
   return pool.query(queryString, queryParams)
-  .then((res) => res.rows)
+  .then((res) => {
+  return res.rows
+})
     .catch((err)=> {
       console.log(err.message)
     });
@@ -162,9 +162,19 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `;
+  const queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
+
+  return pool.query(queryString, queryParams)
+  .then((res) => console.log(res.rows[0]))
+  .catch((err)=>{
+    console.log(err.message)
+    return null
+  })
+  
 }
 exports.addProperty = addProperty;
